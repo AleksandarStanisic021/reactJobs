53738fda4e51d789934c801ef78d0d99e22708c9
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-28 17:59:36 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 
-const App = () => {
-  const names = ["sasa", "pera", "mika"];
-  const [logedin, setLogedin] = useState(false);
+import Navbar from "./comps/Navbar.jsx";
 
+const App = () => {
   return (
     <>
-      <h1 className="p-2">hello</h1>
-
-      <div className="bg-center">
-        {names.map((n) => (
-          <li key={n}> {n}</li>
-        ))}
-        <button
-          style={{ background: "red", color: "white", padding: "1rem" }}
-          onClick={() => setLogedin(!logedin)}>
-          SHOW/HIDE
-        </button>
-      </div>
-      {logedin && <h1>Sasa</h1>}
+      <Navbar />
     </>
   );
 };
-
 export default App;
