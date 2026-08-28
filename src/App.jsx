446b4b7bc79950535e75ce90f615adef200a496c
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-28 17:12:55 +0200

    add list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 
 const App = () => {
+  const names = ["sasa", "pera", "mika"];
+
   return (
     <>
       <h1 className="p-2">hello</h1>
       <h1 className="p-4">hello</h1>
       <h1 className="p-6">hello</h1>
+      <div className="bg-center">
+        {names.map((n) => (
+          <li key={n}> {n}</li>
+        ))}
+      </div>
     </>
   );
 };
