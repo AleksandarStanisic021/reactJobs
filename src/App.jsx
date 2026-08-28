ce079b6fe058405d4c8903af2fb85ec67ee97cfb
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-28 17:02:16 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
-      <p>this is tw test</p>
+      <h1 className="p-2">hello</h1>
+      <h1 className="p-4">hello</h1>
+      <h1 className="p-6">hello</h1>
     </>
   );
 };
