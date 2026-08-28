98607e580b4d9473e2595969468ad9f05e8bc156
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-28 17:23:40 +0200

    feat: implement login state management with conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
   const names = ["sasa", "pera", "mika"];
-  const logenin = true;
+  const [logedin, setLogedin] = useState(false);
 
   return (
     <>
       <h1 className="p-2">hello</h1>
-      <h1 className="p-4">hello</h1>
-      <h1 className="p-6">hello</h1>
+
       <div className="bg-center">
         {names.map((n) => (
           <li key={n}> {n}</li>
         ))}
+        <button onClick={() => setLogedin(!logedin)}>SHOW/HIDE</button>
       </div>
-      {logenin && <h1>Sasa</h1>}
+      {logedin && <h1>Sasa</h1>}
     </>
   );
 };
