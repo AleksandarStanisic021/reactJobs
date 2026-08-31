26f71c1ee8b5141442d8c79a1a260c3426882cf1
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-31 13:10:18 +0200

    feat: add greet function and button to UserCard for user interaction

--- src/comps/UserCard.jsx
+++ src/comps/UserCard.jsx
@@ -1,6 +1,10 @@
 import Card from "../Card";
 
 const UserCard = ({ name, role }) => {
+  const greet = (name) => {
+    console.log(`hi ${name}`);
+  };
+
   return (
     <Card>
       <div
@@ -12,6 +16,12 @@ const UserCard = ({ name, role }) => {
         <p>Name {name}</p>
         <p>Position {role}</p>
       </div>
+      <button
+        onClick={(e) => {
+          greet(name);
+        }}>
+        hello
+      </button>
     </Card>
   );
 };
