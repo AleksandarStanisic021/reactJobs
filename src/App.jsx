631139cda7c88eb0817557abd0e4290e2c0be78d
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-31 11:05:29 +0200

    workin on styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 
+const styles = {
+  backgroundColor: "red",
+  color: "white",
+};
+
 const App = () => {
-  return (
+  n(
     <>
-      <h1>Hi all</h1>
+      <h1 style={styles}>Hi all</h1>
       <p>How's going?</p>
       <p>Sum: {2 + 3}</p>
-    </>
+    </>,
   );
 };
 export default App;
